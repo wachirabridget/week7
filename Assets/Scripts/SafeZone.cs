302dfb720c2c0645bef76d469ee10f0883fea396
wachirabridget <wanjirubridget10@gmail.com>
2025-11-01 00:35:20 +0300

    m

---
+++ Assets/Scripts/SafeZone.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class SafeZone : MonoBehaviour
+{
+    // This script only serves to tag a platform segment as safe for Precision Mode.
+    // All collision logic is handled in BallController.cs.
+}
